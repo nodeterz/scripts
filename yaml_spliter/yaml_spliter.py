1ffb558788aec204c89c18f46a11cb5c328aaaf5
Alireza Ghasemi <alirezagh76@gmail.com>
2019-08-07 13:45:33 +0200

    new yaml_splitter is added.

---
+++ yaml_spliter/yaml_spliter.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+import yaml
+import argparse
+str1 = "This script reads input.yaml and deletes the specific structures from input file"
+parser = argparse.ArgumentParser(description=str1)
+parser.add_argument('fn_inp', action='store' ,type=str, help="Name of the input file in yaml format")
+parser.add_argument('str_num', action='store' ,type=int, help="Name of desired conf in each file.")
+args=parser.parse_args()
+try:
+    from yaml import CLoader as Loader
+except ImportError:
+    from yaml import Loader
+stream=open(args.fn_inp)
+dict_list = list(yaml.load_all(stream, Loader=Loader))
+
+for count,conf in enumerate(dict_list):
+    print(count)
+    file_num = count/args.str_num
+    fn_out = "tt%5.5d.yaml"%file_num
+    output_stream=open(fn_out,"a+")
+    output_stream.write('---\n')
+    yaml.dump(dict_list[count],output_stream)
+
+
+
+
+#del_list = yaml.load(stream)
+#for i,d in enumerate(del_list):
+#    del_list[i]=int(d)-1
+#
+#conf_list=list(range(len(dict_list)))
+#
+#conf_keep_list=list(set(conf_list) - set(del_list))
+#
+#for conf in conf_keep_list:
+#    output_stream.write('---\n')
+#    yaml.dump(dict_list[conf],output_stream)
