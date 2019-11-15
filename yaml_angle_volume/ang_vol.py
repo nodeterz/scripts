ad60d40a9bef530e3443ffeebc379f639b1fc519
nodeterz <[rahmatizad@gmail.com]>
2019-11-15 17:32:05 +0330

    ratio is added to ang_vol.

--- yaml_angle_volume/ang_vol.py
+++ yaml_angle_volume/ang_vol.py
@@ -8,6 +8,9 @@ def angle(v1, v2):
     return np.arccos(np.inner(v1,v2)/(np.linalg.norm(v1)*np.linalg.norm(v2)))*180.0/np.pi
 def volume(v1, v2, v3):
     return np.inner(np.cross(v1,v2),v3)
+def ratio(v1, v2):
+    return np.linalg.norm(v1)/np.linalg.norm(v2)
+
 
 str1 = "This script reads a yaml file and prints cell margin distance from LR/TB/FB"
 parser = argparse.ArgumentParser(description=str1)
@@ -22,8 +25,7 @@ dict_list = list(yaml.load_all(stream, Loader=Loader))
 v1 = [0, 0 ,0]
 v2 = [0, 0 ,0]
 v3 = [0, 0 ,0]
-f='false'
-t='true'
+print 'iconf','\t', 'angle(v1,v2)','\t', 'angle(v2,v3)','\t', 'angle(v1,v3)','\t', 'volume(v1, v2, v3)','\t','ratio(v1, v2)','\t','ratio(v1,v3)','\t','ratio(v2,v3)'
 for iconf,conf in enumerate(dict_list):
     v1[0]=conf['conf']['cell'][0][0]
     v1[1]=conf['conf']['cell'][0][1]
@@ -34,4 +36,4 @@ for iconf,conf in enumerate(dict_list):
     v3[0]=conf['conf']['cell'][2][0]
     v3[1]=conf['conf']['cell'][2][1]
     v3[2]=conf['conf']['cell'][2][2]
-    print angle(v1,v2),angle(v2,v3),angle(v1,v3),volume(v1, v2, v3)
+    print("%5d \t %10.3f \t %10.3f \t %10.3f \t %10.3f \t %10.3f \t %10.3f \t %10.3f" %(iconf+1, angle(v1,v2), angle(v2,v3), angle(v1,v3), volume(v1, v2, v3),ratio(v1, v2),ratio(v1,v3),ratio(v2,v3)))
