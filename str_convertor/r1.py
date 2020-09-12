55d75485f4bbac219c186a51f7a8692de11f3899
nodeterz <rahmatizad@gmail.com>
2020-09-12 09:48:24 +0430

    str_convertor mistake corrected

--- str_convertor/r1.py
+++ str_convertor/r1.py
@@ -53,40 +53,88 @@ for i in range(int(dict_list[0]['conf']['nat'])):
     #print dict_list[0]['conf']['coord'][i][3].strip()
     if dict_list[0]['conf']['coord'][i][3].strip()=='He':
         He=True
+        if dict_list[0]['conf']['coord'][i][0] > (dict_list[0]['conf']['cell'][0][0]):
+            dict_list[0]['conf']['coord'][i][0]=dict_list[0]['conf']['coord'][i][0]-dict_list[0]['conf']['cell'][0][0]
+        if dict_list[0]['conf']['coord'][i][1] > (dict_list[0]['conf']['cell'][1][1]):
+            dict_list[0]['conf']['coord'][i][1]=dict_list[0]['conf']['coord'][i][1]-dict_list[0]['conf']['cell'][1][1]
+        if dict_list[0]['conf']['coord'][i][2] > (dict_list[0]['conf']['cell'][2][2]):
+            dict_list[0]['conf']['coord'][i][2]=dict_list[0]['conf']['coord'][i][2]-dict_list[0]['conf']['cell'][2][2]
         xat_He=xat_He+dict_list[0]['conf']['coord'][i][0]
         yat_He=yat_He+dict_list[0]['conf']['coord'][i][1]
         zat_He=zat_He+dict_list[0]['conf']['coord'][i][2]
     elif dict_list[0]['conf']['coord'][i][3].strip()=='Ne':
+        if dict_list[0]['conf']['coord'][i][0] > (dict_list[0]['conf']['cell'][0][0]):
+            dict_list[0]['conf']['coord'][i][0]=dict_list[0]['conf']['coord'][i][0]-dict_list[0]['conf']['cell'][0][0]
+        if dict_list[0]['conf']['coord'][i][1] > (dict_list[0]['conf']['cell'][1][1]):
+            dict_list[0]['conf']['coord'][i][1]=dict_list[0]['conf']['coord'][i][1]-dict_list[0]['conf']['cell'][1][1]
+        if dict_list[0]['conf']['coord'][i][2] > (dict_list[0]['conf']['cell'][2][2]):
+            dict_list[0]['conf']['coord'][i][2]=dict_list[0]['conf']['coord'][i][2]-dict_list[0]['conf']['cell'][2][2]
         Ne=True
         xat_Ne=xat_Ne+dict_list[0]['conf']['coord'][i][0]
         yat_Ne=yat_Ne+dict_list[0]['conf']['coord'][i][1]
         zat_Ne=zat_Ne+dict_list[0]['conf']['coord'][i][2]
     elif dict_list[0]['conf']['coord'][i][3].strip()=='Ar':
+        if dict_list[0]['conf']['coord'][i][0] > (dict_list[0]['conf']['cell'][0][0]):
+            dict_list[0]['conf']['coord'][i][0]=dict_list[0]['conf']['coord'][i][0]-dict_list[0]['conf']['cell'][0][0]
+        if dict_list[0]['conf']['coord'][i][1] > (dict_list[0]['conf']['cell'][1][1]):
+            dict_list[0]['conf']['coord'][i][1]=dict_list[0]['conf']['coord'][i][1]-dict_list[0]['conf']['cell'][1][1]
+        if dict_list[0]['conf']['coord'][i][2] > (dict_list[0]['conf']['cell'][2][2]):
+            dict_list[0]['conf']['coord'][i][2]=dict_list[0]['conf']['coord'][i][2]-dict_list[0]['conf']['cell'][2][2]
         Ar=True
         xat_Ar=xat_Ar+dict_list[0]['conf']['coord'][i][0]
         yat_Ar=yat_Ar+dict_list[0]['conf']['coord'][i][1]
         zat_Ar=zat_Ar+dict_list[0]['conf']['coord'][i][2]
     elif dict_list[0]['conf']['coord'][i][3].strip()=='Kr':
+        if dict_list[0]['conf']['coord'][i][0] > (dict_list[0]['conf']['cell'][0][0]):
+            dict_list[0]['conf']['coord'][i][0]=dict_list[0]['conf']['coord'][i][0]-dict_list[0]['conf']['cell'][0][0]
+        if dict_list[0]['conf']['coord'][i][1] > (dict_list[0]['conf']['cell'][1][1]):
+            dict_list[0]['conf']['coord'][i][1]=dict_list[0]['conf']['coord'][i][1]-dict_list[0]['conf']['cell'][1][1]
+        if dict_list[0]['conf']['coord'][i][2] > (dict_list[0]['conf']['cell'][2][2]):
+            dict_list[0]['conf']['coord'][i][2]=dict_list[0]['conf']['coord'][i][2]-dict_list[0]['conf']['cell'][2][2]
         Kr=True
         xat_Kr=xat_Kr+dict_list[0]['conf']['coord'][i][0]
         yat_Kr=yat_Kr+dict_list[0]['conf']['coord'][i][1]
         zat_Kr=zat_Kr+dict_list[0]['conf']['coord'][i][2]
     elif dict_list[0]['conf']['coord'][i][3].strip()=='Xe':
+        if dict_list[0]['conf']['coord'][i][0] > (dict_list[0]['conf']['cell'][0][0]):
+            dict_list[0]['conf']['coord'][i][0]=dict_list[0]['conf']['coord'][i][0]-dict_list[0]['conf']['cell'][0][0]
+        if dict_list[0]['conf']['coord'][i][1] > (dict_list[0]['conf']['cell'][1][1]):
+            dict_list[0]['conf']['coord'][i][1]=dict_list[0]['conf']['coord'][i][1]-dict_list[0]['conf']['cell'][1][1]
+        if dict_list[0]['conf']['coord'][i][2] > (dict_list[0]['conf']['cell'][2][2]):
+            dict_list[0]['conf']['coord'][i][2]=dict_list[0]['conf']['coord'][i][2]-dict_list[0]['conf']['cell'][2][2]
         Xe=True
         xat_Xe=xat_Xe+dict_list[0]['conf']['coord'][i][0]
         yat_Xe=yat_Xe+dict_list[0]['conf']['coord'][i][1]
         zat_Xe=zat_Xe+dict_list[0]['conf']['coord'][i][2]
     elif dict_list[0]['conf']['coord'][i][3].strip()=='Rn':
+        if dict_list[0]['conf']['coord'][i][0] > (dict_list[0]['conf']['cell'][0][0]):
+            dict_list[0]['conf']['coord'][i][0]=dict_list[0]['conf']['coord'][i][0]-dict_list[0]['conf']['cell'][0][0]
+        if dict_list[0]['conf']['coord'][i][1] > (dict_list[0]['conf']['cell'][1][1]):
+            dict_list[0]['conf']['coord'][i][1]=dict_list[0]['conf']['coord'][i][1]-dict_list[0]['conf']['cell'][1][1]
+        if dict_list[0]['conf']['coord'][i][2] > (dict_list[0]['conf']['cell'][2][2]):
+            dict_list[0]['conf']['coord'][i][2]=dict_list[0]['conf']['coord'][i][2]-dict_list[0]['conf']['cell'][2][2]
         Rn=True
         xat_Rn=xat_Rn+dict_list[0]['conf']['coord'][i][0]
         yat_Rn=yat_Rn+dict_list[0]['conf']['coord'][i][1]
         zat_Rn=zat_Rn+dict_list[0]['conf']['coord'][i][2]
     elif dict_list[0]['conf']['coord'][i][3].strip()=='Lu':
+        if dict_list[0]['conf']['coord'][i][0] > (dict_list[0]['conf']['cell'][0][0]):
+            dict_list[0]['conf']['coord'][i][0]=dict_list[0]['conf']['coord'][i][0]-dict_list[0]['conf']['cell'][0][0]
+        if dict_list[0]['conf']['coord'][i][1] > (dict_list[0]['conf']['cell'][1][1]):
+            dict_list[0]['conf']['coord'][i][1]=dict_list[0]['conf']['coord'][i][1]-dict_list[0]['conf']['cell'][1][1]
+        if dict_list[0]['conf']['coord'][i][2] > (dict_list[0]['conf']['cell'][2][2]):
+            dict_list[0]['conf']['coord'][i][2]=dict_list[0]['conf']['coord'][i][2]-dict_list[0]['conf']['cell'][2][2]
         Lu=True
         xat_Lu=xat_Lu+dict_list[0]['conf']['coord'][i][0]
         yat_Lu=yat_Lu+dict_list[0]['conf']['coord'][i][1]
         zat_Lu=zat_Lu+dict_list[0]['conf']['coord'][i][2]
     elif dict_list[0]['conf']['coord'][i][3].strip()=='Lr':
+        if dict_list[0]['conf']['coord'][i][0] > (dict_list[0]['conf']['cell'][0][0]):
+            dict_list[0]['conf']['coord'][i][0]=dict_list[0]['conf']['coord'][i][0]-dict_list[0]['conf']['cell'][0][0]
+        if dict_list[0]['conf']['coord'][i][1] > (dict_list[0]['conf']['cell'][1][1]):
+            dict_list[0]['conf']['coord'][i][1]=dict_list[0]['conf']['coord'][i][1]-dict_list[0]['conf']['cell'][1][1]
+        if dict_list[0]['conf']['coord'][i][2] > (dict_list[0]['conf']['cell'][2][2]):
+            dict_list[0]['conf']['coord'][i][2]=dict_list[0]['conf']['coord'][i][2]-dict_list[0]['conf']['cell'][2][2]
         Lr=True
         xat_Lr=xat_Lr+dict_list[0]['conf']['coord'][i][0]
         yat_Lr=yat_Lr+dict_list[0]['conf']['coord'][i][1]
